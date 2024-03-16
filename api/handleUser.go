6262424f3902484e4488e37f578095a845301913
abidkiller <abidahsan1700@gmail.com>
2024-03-17 04:07:55 +0600

    project init

---
+++ api/handleUser.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"go/types"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func HandleUser(c *fiber.Ctx) error {
+	u := types.
+
+	//return c.JSON(map[string]string{"data": "abid Ahsan"})
+}
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/abidkiller/hotel_reservation_backend/api"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 
 	app := fiber.New()
+	apiv1 := app.Group("/api/v1")
 	app.Get("/hi", handleReq)
+	apiv1.Get("/hi", handleReq)
+	apiv1.Get("/user", api.HandleUser)
 	app.Listen(":5000")
 }
 
